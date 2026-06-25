7663fb447b29caa2263aeb0e800ca5235c1f0bc3
HuyDevGame1402 <huyco14022004@gmail.com>
2026-06-25 11:12:59 +0700

    VFX Enemy Attack

--- Assets/Scripts/Enemy/EnemyVFXManager.cs
+++ Assets/Scripts/Enemy/EnemyVFXManager.cs
@@ -4,9 +4,14 @@ using UnityEngine.VFX;
 public class EnemyVFXManager : MonoBehaviour
 {
     public VisualEffect footStep;
+    public VisualEffect attackVFX;
 
     public void BurstFootStep()
     {
         footStep.SendEvent("OnPlay");
+    }
+    public void PlayAttackVFX()
+    {
+        attackVFX.SendEvent("OnPlay");
     }
 }
