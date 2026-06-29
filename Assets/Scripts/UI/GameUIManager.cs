4f2db664b4de8e6211743fad60d6abccfd46e439
HuyDevGame1402 <huyco14022004@gmail.com>
2026-06-29 13:06:56 +0700

    Enum state game UI

--- Assets/Scripts/UI/GameUIManager.cs
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -9,6 +9,20 @@ public class GameUIManager : MonoBehaviour
     [SerializeField] private Slider healthSlider;
     private Health health;
 
+    [SerializeField] private GameObject uiPause;
+    [SerializeField] private GameObject uiGameOver;
+    [SerializeField] private GameObject uiGameIsFinish;
+
+    private enum GameUIState
+    {
+        GamePlay,
+        GamePause,
+        GameOver,
+        GameIsFinish,
+    }
+
+    private GameUIState currentState;
+
     private void Awake()
     {
         health = GameManager.Instance.GetPlayerCharacter().GetComponent<Health>();
@@ -25,4 +39,46 @@ public class GameUIManager : MonoBehaviour
     {
         healthSlider.value = ((Health.OnDamageClass)e).healthPercentage;
     }
+
+    private void SwitchUIState(GameUIState state)
+    {
+        uiPause.SetActive(false);
+        uiGameOver.SetActive(false);
+        uiGameIsFinish.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        switch (state)
+        {
+            case GameUIState.GamePlay:
+                break;
+
+            case GameUIState.GamePause:
+                Time.timeScale = 0f;
+                uiPause.SetActive(true);
+                break;
+
+            case GameUIState.GameOver:
+                uiGameOver.SetActive(true);
+                break;
+
+            case GameUIState.GameIsFinish:
+                uiGameIsFinish.SetActive(true);
+                break;
+        }
+        currentState = state;
+    }
+
+    public void TogglePauseUI()
+    {
+        if(currentState == GameUIState.GamePlay)
+        {
+            SwitchUIState(GameUIState.GamePause);
+        }
+        else if(currentState == GameUIState.GamePause)
+        {
+            SwitchUIState(GameUIState.GamePlay);
+        }
+    }
+
 }
